0924aeffa5887a7a99ab02141a907e15d3f4385e
AurelienChoffay <94602872+AurelienChoffay@users.noreply.github.com>
2025-06-03 14:29:55 +0200

    feat : add filter request API (#7)

--- src/MuscuAPI.Application/Services/Interfaces/IMuscleService.cs
+++ src/MuscuAPI.Application/Services/Interfaces/IMuscleService.cs
@@ -5,9 +5,10 @@ namespace MuscuAPI.Application.Services.Interfaces;
 
 public interface IMuscleService
 {
-    Task<PagedResult<MuscleDto>> GetMusclesAsync(PaginationParams paginationParams, int? groupeMusculaireId = null);
+    Task<PagedResult<MuscleDto>> GetMusclesAsync(MuscleFilterParams filterParams);
     Task<MuscleDto?> GetMuscleByIdAsync(int id);
     Task<MuscleDto> CreateMuscleAsync(CreateMuscleDto createMuscleDto);
     Task<MuscleDto?> UpdateMuscleAsync(int id, UpdateMuscleDto updateMuscleDto);
     Task<bool> DeleteMuscleAsync(int id);
+    Task<IEnumerable<MuscleDto>> SearchMusclesAsync(string searchTerm);
 }

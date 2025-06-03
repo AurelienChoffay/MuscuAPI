1ebcc48d0fbd3990c5321f8efaf8d1b52b6a6c5d
AurelienChoffay <94602872+AurelienChoffay@users.noreply.github.com>
2025-06-03 14:12:57 +0200

    feat : implementation of the Muscle controller (#6)

---
+++ src/MuscuAPI.Application/Services/Interfaces/IMuscleService.cs
@@ -0,0 +1,13 @@
+﻿using MuscuAPI.Application.DTOs.Common;
+using MuscuAPI.Application.DTOs.Muscle;
+
+namespace MuscuAPI.Application.Services.Interfaces;
+
+public interface IMuscleService
+{
+    Task<PagedResult<MuscleDto>> GetMusclesAsync(PaginationParams paginationParams, int? groupeMusculaireId = null);
+    Task<MuscleDto?> GetMuscleByIdAsync(int id);
+    Task<MuscleDto> CreateMuscleAsync(CreateMuscleDto createMuscleDto);
+    Task<MuscleDto?> UpdateMuscleAsync(int id, UpdateMuscleDto updateMuscleDto);
+    Task<bool> DeleteMuscleAsync(int id);
+}

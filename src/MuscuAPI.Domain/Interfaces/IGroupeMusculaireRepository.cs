5260ae84e3cfc273c771a90442cde66256391f8d
AurelienChoffay <auchoffay@gmail.com>
2025-06-03 13:45:31 +0200

    feat : implementation repository muscle

---
+++ src/MuscuAPI.Domain/Interfaces/IGroupeMusculaireRepository.cs
@@ -0,0 +1,10 @@
+﻿using MuscuAPI.Domain.Entities;
+
+namespace MuscuAPI.Domain.Interfaces;
+
+public interface IGroupeMusculaireRepository : IRepository<GroupeMusculaire>
+{
+    Task<IEnumerable<GroupeMusculaire>> GetActiveGroupesAsync();
+    Task<GroupeMusculaire?> GetByIdWithMusclesAsync(int id);
+    Task<bool> HasMusclesAsync(int id);
+}
